90a70dde230d96ed766937dc0ef92451e6ef249a
rmard <rmardn@gmail.com>
2018-10-25 22:48:53 -0300

    Book and Bookshelf components
    Added 2 new components and the update functionality

---
+++ src/Book.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class Book extends React.Component {
+  render = () => {
+    console.log(this.props);
+    var book = this.props.book;
+    if(!book)
+        return null;
+    else
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+          <div className="book-shelf-changer">
+            <select onChange={(event)=>this.props.updateBook(event, book)} value={book.shelf}>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
+      </div>
+    );
+  }
+}
+
+export default Book;
---
+++ src/Bookshelf.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Book from './Book'
+
+class Bookshelf extends React.Component {
+  
+  render = () => {
+    const { title, books, updateBook } = this.props;
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map((book)=>(
+              <li key={book.id}>
+                <Book book={book} updateBook={updateBook}/>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Bookshelf
